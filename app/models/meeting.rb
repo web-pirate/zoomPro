e88413a40730b3622ac267c5e62d7b6299c95c21
Manish <manishsinghdewas@gmail.com>
2025-01-31 02:06:27 +0530

    goodNight Changes

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -6,7 +6,6 @@ class Meeting < ApplicationRecord
     validates :topic, :start_time, :duration, presence: true
 end
 
-
 # Code to create the Meeting model:
 # rails generate model Meeting user:references topic:string start_time:datetime duration:integer recording_url:string
 # rails db:migrate
